ab18424b4945f94110a3a33f6f4a3092c37a9b5f
tj <tj@data-driven.de>
2012-11-11 08:16:00 +0100

    K function improved, Q function written in pil-edit.el.

--- pil-edit.el
+++ pil-edit.el
@@ -1,56 +1,74 @@
+;; * FIXME: 
+;; ** recognition of unit via point not region
+;; ** bind C-u C-x C-c to one key (F??)
+
+
 (defun pil-edit-K ()
-  "Write symbol at point with line number in last line of edit file.
+  "Write symbol at point with line number in last line of edit-buffer.
 
 If the symbol is a transient symbol, write it with double-quotes,
 otherwise as unquoted word. The output-format is: 
 
 \(<line-number> <symbol>\)
-
  e.g.
-
-\(50  edit)
+\(50  edit\)
 \(56 \"edit\"\)
 
-when point is on the edit or (transient) \"edit\" symbol in the
-picolisp sourcefile edit.l and `pil-edit-K' is called (the
-line-numbers may be different in your version of edit.l)."
+when point is on the edit or \(transient\) \"edit\" symbol in the
+PicoLisp sourcefile edit.l and `pil-edit-K' is called (the
+line-numbers may be different in your version of edit.l).
+
+Recognition of transient symbols works by getting the
+text-property 'face' at point and checking if it is equal to
+'font-lock-string-face'. Thus, this function works correctly only
+if the edit-buffer is in an Emacs major-mode that fontifies
+strings with 'font-lock-string-face' \(like `picolisp-mode'
+does\)."
+
   (interactive)
   (save-excursion
     (save-restriction
       (widen)
-      (let* ((pos (point))
-             (word (thing-at-point 'word))  ; FIXME better use 'symbol ?
+      (unless (mark 'FORCE)
+        (forward-word)
+        (forward-word -1)
+        (mark-word))
+      (let* ((thing (thing-at-point 'word))  ; FIXME better use 'symbol ?
              (unit (get-selection-or-unit 'word))
              (line (line-number-at-pos))
              (transient-p
-              (string-equal (get-text-property pos 'face)
+              (string-equal (get-text-property (point) 'face)
                             "font-lock-string-face"))
              (k-list nil))
-        (setq k-list
-              (list line
-                    (if transient-p
-                        (elt unit 0)
-                      (make-symbol (elt unit 0)))))
-        (message "k-list: %S transient: %S" k-list transient-p)
+        (setq k-list (list line
+                           (if transient-p
+                               (elt unit 0)
+                             (make-symbol (elt unit 0)))))
+        (message "K-list: %S transient: %S" k-list transient-p)
         (goto-char (max-char))
-        (beginning-of-line)
-        (insert (format "%S" k-list))))))
+        (newline)
+        (insert (format "%S" k-list))
+        (save-buffers-kill-terminal 1)))))
+
+
+(defun pil-edit-Q ()
+  "Write '(0)' in last line of PicoLisp edit-buffer."
+  (interactive)
+  (save-excursion
+    (save-restriction
+      (widen)
+      (goto-char (max-char))
+      (newline)
+      (insert "(0)")
+      (save-buffers-kill-terminal 1))))
       
 
 ;; from: http://ergoemacs.org/emacs/elisp_get-selection-or-unit.html 
-;; ∑ ErgoEmacs ◆ (emacs tutorial ◇ elisp tutorial) ◆ Blog ◆ (emacs
-;; manual ◇ elisp manual) Emacs Lisp Tutorial Emacs Lisp:
-;; get-selection-or-unit GitCafe 中文
-
 ;; Xah Lee, 2011-03-16, …, 2012-07-17
-;; 😸
-
 ;; This page explains the elisp functions get-selection-or-unit ＆
 ;; unit-at-cursor, contained in the package 〔xeu_elisp_util.el〕.
-
 ;; Get the latest version at:
 ;; http://code.google.com/p/ergoemacs/source/browse/packages/xeu_elisp_util.el
-
 ;; For reasons why it's written, see: Emacs Lisp: Using thing-at-point.
 ;; get-selection-or-unit
 
